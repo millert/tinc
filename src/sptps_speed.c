03677cb39872e46de0675629e9b4e9da39335ead
Todd C. Miller <Todd.Miller@courtesan.com>
2017-09-26 08:55:55 -0600

    Make sptps_speed compile and run under mingw

--- src/sptps_speed.c
+++ src/sptps_speed.c
@@ -21,7 +21,6 @@
 #include "utils.h"
 
 #include <getopt.h>
-#include <poll.h>
 
 #include "crypto.h"
 #include "ecdh.h"
@@ -42,7 +41,14 @@ struct timeval now;
 
 static bool send_data(void *handle, uint8_t type, const void *data, size_t len) {
 	int fd = *(int *)handle;
-	send(fd, data, len, 0);
+	size_t nsent = send(fd, data, len, 0);
+	if (nsent != len) {
+		if (nsent == (size_t)-1)
+			fprintf(stderr, "%s: send: %s\n", __func__, sockstrerror(sockerrno));
+		else
+			fprintf(stderr, "%s: short send\n", __func__);
+		return false;
+	}
 	return true;
 }
 
@@ -55,6 +61,10 @@ static void receive_data(sptps_t *sptps) {
 	int fd = *(int *)sptps->handle;
 	size_t len = recv(fd, buf, sizeof(buf), 0);
 
+	if (len == (size_t)-1) {
+		fprintf(stderr, "%s: recv: %s\n", __func__, sockstrerror(sockerrno));
+		return;
+	}
 	while(len) {
 		size_t done = sptps_receive_data(sptps, bufp, len);
 
@@ -92,8 +102,8 @@ static bool clock_countto(double seconds) {
 
 static struct option const long_options[] = {
 	{"cipher", required_argument, NULL, 'c'},
+	{"help", no_argument, NULL, 'h'},
 	{"key-type", required_argument, NULL, 'k'},
-	{"help", no_argument, NULL, 1},
 	{NULL, 0, NULL, 0}
 };
 
@@ -108,6 +118,174 @@ static void usage() {
 			"\n");
 	fprintf(stderr, "Report bugs to tinc@tinc-vpn.org.\n");
 }
+
+#ifdef HAVE_MINGW
+static int set_nonblock(int sock) {
+#ifdef O_NONBLOCK
+        int flags = fcntl(sock, F_GETFL);
+
+	flags |= O_NONBLOCK;
+        if(fcntl(sock, F_SETFL, flags) == -1)
+		return -1;
+#else
+        unsigned long arg = 1;
+
+        if(ioctlsocket(sock, FIONBIO, &arg) != 0)
+		return -1;
+#endif
+	return 0;
+}
+
+static int unset_nonblock(int sock) {
+#ifdef O_NONBLOCK
+        int flags = fcntl(sock, F_GETFL);
+
+	flags &= ~O_NONBLOCK;
+        if(fcntl(sock, F_SETFL, flags) == -1)
+		return -1;
+#else
+        unsigned long arg = 0;
+
+        if(ioctlsocket(sock, FIONBIO, &arg) != 0)
+		return -1;
+#endif
+	return 0;
+}
+
+#define LOCAL_PORT	65432
+
+#ifdef HAVE_MINGW
+# define CONNECT_IN_PROGRESS	 WSAEWOULDBLOCK
+#else
+# define CONNECT_IN_PROGRESS	 EINPROGRESS
+#endif
+
+/* Connect fd[0] <-> fd[1], TCP */
+static int connect_sockets_tcp(int *fd, struct sockaddr_in *sin)
+{
+	if (set_nonblock(fd[0])) {
+		fprintf(stderr, "Could not make socket nonblocking: %s\n", sockstrerror(sockerrno));
+		return -1;
+	}
+	if (listen(fd[1], 1) == -1) {
+		fprintf(stderr, "Could not listen: %s\n", sockstrerror(sockerrno));
+		return -1;
+	}
+	if (connect(fd[0], (struct sockaddr *)sin, sizeof(*sin)) == -1) {
+		if (sockerrno != CONNECT_IN_PROGRESS) {
+			fprintf(stderr, "Could not connect sockets: %s\n", sockstrerror(sockerrno));
+			return -1;
+		}
+	}
+	socklen_t sinlen = sizeof(*sin);
+	int sock = accept(fd[1], (struct sockaddr *)sin, &sinlen);
+	if (sock == -1) {
+		fprintf(stderr, "Could not accept socket: %s\n", sockstrerror(sockerrno));
+		return -1;
+	}
+	close(fd[1]);
+	fd[1] = sock;
+
+	int errnum;
+	socklen_t len = sizeof(errnum);
+	getsockopt(fd[0], SOL_SOCKET, SO_ERROR, (void *)&errnum, &len);
+	if (errnum != 0) {
+		fprintf(stderr, "connect error: %s\n", sockstrerror(sockerrno));
+		return -1;
+	}
+	unset_nonblock(fd[0]);
+
+	int option = 1;
+#if defined(IPPROTO_TCP) && defined(TCP_NODELAY)
+	setsockopt(fd[0], IPPROTO_TCP, TCP_NODELAY, (void *)&option, sizeof option);
+	setsockopt(fd[1], IPPROTO_TCP, TCP_NODELAY, (void *)&option, sizeof option);
+#endif
+
+#if defined(IPPROTO_IP) && defined(IP_TOS) && defined(IPTOS_LOWDELAY)
+	option = IPTOS_LOWDELAY;
+	setsockopt(fd[0], IPPROTO_IP, IP_TOS, (void *)&option, sizeof option);
+	setsockopt(fd[1], IPPROTO_IP, IP_TOS, (void *)&option, sizeof option);
+#endif
+
+	return 0;
+}
+
+/* Connect fd[0] <-> fd[1], UDP */
+static int connect_sockets_udp(int *fd, struct sockaddr_in *sin)
+{
+	struct sockaddr_in sin2 = *sin;
+	sin2.sin_port = htons(LOCAL_PORT + 1);
+	if (bind(fd[0], (struct sockaddr *)&sin2, sizeof(sin2)) == -1) {
+		fprintf(stderr, "Could not bind socket: %s\n", sockstrerror(sockerrno));
+		return -1;
+	}
+
+	/* We need to explicitly connect each direction. */
+	if (connect(fd[0], (struct sockaddr *)sin, sizeof(*sin)) == -1 ||
+	    connect(fd[1], (struct sockaddr *)&sin2, sizeof(sin2)) == -1) {
+		fprintf(stderr, "Could not connect sockets: %s\n", sockstrerror(sockerrno));
+		return -1;
+	}
+
+	return 0;
+}
+
+static int socketpair(int domain, int type, int proto, int *fd)
+{
+	int serrno;
+
+	struct sockaddr_in sin;
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+	sin.sin_port = htons(LOCAL_PORT);
+
+	fd[0] = socket(AF_INET, type, proto);
+	fd[1] = socket(AF_INET, type, proto);
+	if (fd[0] == -1 || fd[1] == -1) {
+		fprintf(stderr, "Could not create a socket: %s\n", sockstrerror(sockerrno));
+		goto bad;
+	}
+
+#if defined(SOL_SOCKET) && defined(SO_REUSEPORT)
+	int option = 1;
+	setsockopt(fd[1], SOL_SOCKET, SO_REUSEPORT, &option, sizeof(option));
+#endif
+
+	if (bind(fd[1], (struct sockaddr *)&sin, sizeof(sin)) == -1) {
+		fprintf(stderr, "Could not bind socket: %s\n", sockstrerror(sockerrno));
+		goto bad;
+	}
+
+	switch (type) {
+		case SOCK_STREAM:
+			if (connect_sockets_tcp(fd, &sin) == -1)
+				goto bad;
+			break;
+		case SOCK_DGRAM:
+			if (connect_sockets_udp(fd, &sin) == -1)
+				goto bad;
+			break;
+		default:
+			fprintf(stderr, "Unknown socket type %d\n", type);
+			goto bad;
+	}
+
+	return 0;
+bad:
+	serrno = errno;
+	if (fd[0] != -1)
+		close(fd[0]);
+	if (fd[1] != -1)
+		close(fd[1]);
+	errno = serrno;
+	return -1;
+}
+#endif
+
+#ifndef MAX
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#endif
 
 int main(int argc, char *argv[]) {
 	ecdsa_t *key1, *key2;
@@ -134,6 +312,10 @@ int main(int argc, char *argv[]) {
 					return 1;
 				}
 				break;
+
+			case 'h': /* help */
+				usage();
+				return 0;
 
 			case 'k': /* key type */
 				if (strcasecmp(optarg, "ecdsa") == 0) {
@@ -165,6 +347,12 @@ int main(int argc, char *argv[]) {
 	randomize(buf1, sizeof(buf1));
 	randomize(buf2, sizeof(buf2));
 	randomize(buf3, sizeof(buf3));
+
+#ifdef HAVE_MINGW
+	static struct WSAData wsa_state;
+	if(WSAStartup(MAKEWORD(2, 2), &wsa_state))
+		return 1;
+#endif
 
 	// Key generation
 
@@ -237,26 +425,36 @@ int main(int argc, char *argv[]) {
 
 	int fd[2];
 
-	/* XXX - use AF_INET for windows */
 	if(socketpair(AF_UNIX, SOCK_STREAM, 0, fd)) {
 		fprintf(stderr, "Could not create a UNIX socket pair: %s\n", sockstrerror(sockerrno));
 		return 1;
 	}
 
-	/* XXX - use select for windows */
-	struct pollfd pfd[2] = {{fd[0], POLLIN}, {fd[1], POLLIN}};
+	fd_set rfds;
+	FD_ZERO(&rfds);
+	struct timeval timo;
+	int maxfd = MAX(fd[0], fd[1]);
 
 	fprintf(stderr, "SPTPS/TCP authenticate for %lg seconds: ", duration);
 
 	for(clock_start(); clock_countto(duration);) {
-		sptps_start(&sptps1, fd + 0, true, false, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record);
-		sptps_start(&sptps2, fd + 1, false, false, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record);
-		while(poll(pfd, 2, 0)) {
-			if(pfd[0].revents) {
+		if(!sptps_start(&sptps1, fd + 0, true, false, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record)) {
+			fprintf(stderr, "sptps_start of sptps1 failed\n");
+			return 1;
+		}
+		if(!sptps_start(&sptps2, fd + 1, false, false, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record)) {
+			fprintf(stderr, "sptps_start of sptps2 failed\n");
+			return 1;
+		}
+		FD_SET(fd[0], &rfds);
+		FD_SET(fd[1], &rfds);
+		timo.tv_sec = 0;
+		timo.tv_usec = 0;
+		while(select(maxfd + 1, &rfds, NULL, NULL, &timo)) {
+			if(FD_ISSET(fd[0], &rfds)) {
 				receive_data(&sptps1);
 			}
-
-			if(pfd[1].revents) {
+			if(FD_ISSET(fd[1], &rfds)) {
 				receive_data(&sptps2);
 			}
 		}
@@ -269,14 +467,23 @@ int main(int argc, char *argv[]) {
 
 	// SPTPS data
 
-	sptps_start(&sptps1, fd + 0, true, false, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record);
-	sptps_start(&sptps2, fd + 1, false, false, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record);
-	while(poll(pfd, 2, 0)) {
-		if(pfd[0].revents) {
+	if(!sptps_start(&sptps1, fd + 0, true, false, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record)) {
+		fprintf(stderr, "sptps_start of sptps1 failed\n");
+		return 1;
+	}
+	if(!sptps_start(&sptps2, fd + 1, false, false, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record)) {
+		fprintf(stderr, "sptps_start of sptps2 failed\n");
+		return 1;
+	}
+	FD_SET(fd[0], &rfds);
+	FD_SET(fd[1], &rfds);
+	timo.tv_sec = 0;
+	timo.tv_usec = 0;
+	while(select(maxfd + 1, &rfds, NULL, NULL, &timo)) {
+		if(FD_ISSET(fd[0], &rfds)) {
 			receive_data(&sptps1);
 		}
-
-		if(pfd[1].revents) {
+		if(FD_ISSET(fd[1], &rfds)) {
 			receive_data(&sptps2);
 		}
 	}
@@ -313,18 +520,28 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Could not create a UNIX socket pair: %s\n", sockstrerror(sockerrno));
 		return 1;
 	}
+	maxfd = MAX(fd[0], fd[1]);
 
 	fprintf(stderr, "SPTPS/UDP authenticate for %lg seconds: ", duration);
 
 	for(clock_start(); clock_countto(duration);) {
-		sptps_start(&sptps1, fd + 0, true, true, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record);
-		sptps_start(&sptps2, fd + 1, false, true, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record);
-		while(poll(pfd, 2, 0)) {
-			if(pfd[0].revents) {
+		if(!sptps_start(&sptps1, fd + 0, true, true, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record)) {
+			fprintf(stderr, "sptps_start of sptps1 failed\n");
+			return 1;
+		}
+		if(!sptps_start(&sptps2, fd + 1, false, true, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record)) {
+			fprintf(stderr, "sptps_start of sptps2 failed\n");
+			return 1;
+		}
+		FD_SET(fd[0], &rfds);
+		FD_SET(fd[1], &rfds);
+		timo.tv_sec = 0;
+		timo.tv_usec = 0;
+		while(select(maxfd + 1, &rfds, NULL, NULL, &timo)) {
+			if(FD_ISSET(fd[0], &rfds)) {
 				receive_data(&sptps1);
 			}
-
-			if(pfd[1].revents) {
+			if(FD_ISSET(fd[1], &rfds)) {
 				receive_data(&sptps2);
 			}
 		}
@@ -337,15 +554,23 @@ int main(int argc, char *argv[]) {
 
 	// SPTPS datagram data
 
-	sptps_start(&sptps1, fd + 0, true, true, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record);
-	sptps_start(&sptps2, fd + 1, false, true, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record);
-
-	while(poll(pfd, 2, 0)) {
-		if(pfd[0].revents) {
+	if(!sptps_start(&sptps1, fd + 0, true, true, ciphertype, key1, key2, "sptps_speed", 11, send_data, receive_record)) {
+		fprintf(stderr, "sptps_start of sptps1 failed\n");
+		return 1;
+	}
+	if(!sptps_start(&sptps2, fd + 1, false, true, ciphertype, key2, key1, "sptps_speed", 11, send_data, receive_record)) {
+		fprintf(stderr, "sptps_start of sptps2 failed\n");
+		return 1;
+	}
+	FD_SET(fd[0], &rfds);
+	FD_SET(fd[1], &rfds);
+	timo.tv_sec = 0;
+	timo.tv_usec = 0;
+	while(select(maxfd + 1, &rfds, NULL, NULL, &timo)) {
+		if(FD_ISSET(fd[0], &rfds)) {
 			receive_data(&sptps1);
 		}
-
-		if(pfd[1].revents) {
+		if(FD_ISSET(fd[1], &rfds)) {
 			receive_data(&sptps2);
 		}
 	}
